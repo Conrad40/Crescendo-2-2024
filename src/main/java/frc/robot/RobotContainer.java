409d66ddecb09519036b836541437e4dae4db2f1
FRC4662-DS3 <bytesizedprogramming@gmail.com>
2024-03-19 16:15:19 -0700

    It finaly stoped going bacwards

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -164,17 +164,18 @@ public class RobotContainer {
                 TrajectoryConfig config = new TrajectoryConfig(
                                 AutoConstants.kMaxSpeedMetersPerSecond,
                                 AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+                                .setReversed(true)
                                 // Add kinematics to ensure max speed is actually obeyed
                                 .setKinematics(DriveConstants.kDriveKinematics);
 
                 // An example trajectory to follow. All units in meters.
                 Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
                                 // Start at the origin facing the +X direction
-                                new Pose2d(0, 0,new Rotation2d(0)),
+                                new Pose2d(0, 0,new Rotation2d(Math.PI)),
                                 // Pass through these two interior waypoints, making an 's' curve path
                                 List.of( new Translation2d(1, new Rotation2d(0))),
                                 // End 3 meters straight ahead of where we started, facing forward
-                                new Pose2d(1.5, 0, new Rotation2d(0)),
+                                new Pose2d(1.5, 0, new Rotation2d(Math.PI)),
                                 config);
 
                 var thetaController = new ProfiledPIDController(
